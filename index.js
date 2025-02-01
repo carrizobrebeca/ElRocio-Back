9156881d0ad9f5ee88a235ef6e80f730564ec529
Rebeca Carrizo Bourlot <carrizob.rebeca@gmail.com>
2025-02-01 16:55:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./src/app")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 10517;
 
 server.listen(PORT, ()=> {
     console.log(`Lintening on port: ${PORT}`);
